ef2680c2c3e8a4bca1617b0f26af283b0eb3e3ae
Rob Snow <snowystinger@gmail.com>
2011-10-11 17:04:26 -0700

    setup for first stage of project

---
+++ src/com/ucla/EE180D/ActivityCapture/EE180DActivity.java
@@ -0,0 +1,13 @@
+package com.ucla.EE180D.ActivityCapture;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EE180DActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
